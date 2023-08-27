a24f45cd5d447c5b65bd0ef9f106e918d31645f5
Tooba Masood <toobamasood786@gmail.com>
2023-08-27 17:52:22 +0500

    first commit

---
+++ first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter value of a=";
+    cin>>a;
+    cout<<"Enter value of b=";
+    cin>>b;
+    sum=a+b;
+    cout<<"sum="<<sum;
+    return 0;
+}
